c8a6404e6177bf57561cd01f3d9ec8dd9d416c98
Faisal Manzer <faisal_manzer@yahoo.in>
2020-06-05 16:44:38 +0530

    Fixed address bug

--- packages/web/src/forms/Address.form.js
+++ packages/web/src/forms/Address.form.js
@@ -1,17 +1,14 @@
 import React, { useState } from 'react';
-import { Form, Col, Row, Button, Divider,Spin } from 'antd';
+import { Form, Col, Row, Button, Divider, Spin } from 'antd';
 import { createAddress, editAddress, retrieveAddress } from '@app/common/api/shipper';
 
 import { formItem } from 'hocs/formItem.hoc';
-import {
-  mainAddressFormFields,
-  addressFormFields
-} from '@app/common/formsFields/address.formFields';
-import Countries from '@app/common/constants/countryAndState'
+import { mainAddressFormFields, addressFormFields } from '@app/common/formsFields/address.formFields';
+import Countries from '@app/common/constants/countryAndState';
 import { useHandelForm } from 'hooks/form';
 
 export const AddressForm = ({ onCancel, onDone, id }) => {
-  const [countryIndex,setCountryIndex] = useState(81)
+  const [countryIndex, setCountryIndex] = useState(81);
   const { form, loading, submit } = useHandelForm({
     create: createAddress,
     edit: editAddress,
@@ -23,43 +20,26 @@ export const AddressForm = ({ onCancel, onDone, id }) => {
     id,
   });
 
-  const fieldChange = () =>{
-    const index = form.getFieldValue('country')
-    setCountryIndex(index);
-  }
+  const fieldChange = () => {
+    const index = form.getFieldValue('country');
+    setCountryIndex(index !== undefined ? index : 81);
+  };
+
   return (
     <Spin spinning={loading}>
-      <Form
-        onFinish={submit}
-        onFieldsChange={fieldChange}
-        layout='vertical'
-        hideRequiredMark>
+      <Form onFinish={submit} onFieldsChange={fieldChange} layout='vertical' hideRequiredMark>
         <Row>
           <Col span={12}>
             {mainAddressFormFields.slice(0, 2).map((item) => (
               <div className='p-2'>
-                {formItem(
-                  item.key,
-                  item.rules,
-                  item.kwargs,
-                  item.type,
-                  item.others,
-                  item.label,
-                )}
+                {formItem(item.key, item.rules, item.kwargs, item.type, item.others, item.label)}
               </div>
             ))}
           </Col>
           <Col span={12}>
             {mainAddressFormFields.slice(2, 4).map((item) => (
               <div className='p-2'>
-                {formItem(
-                  item.key,
-                  item.rules,
-                  item.kwargs,
-                  item.type,
-                  item.others,
-                  item.label,
-                )}
+                {formItem(item.key, item.rules, item.kwargs, item.type, item.others, item.label)}
               </div>
             ))}
           </Col>
@@ -69,42 +49,21 @@ export const AddressForm = ({ onCancel, onDone, id }) => {
           <Col span={8}>
             {addressFormFields.slice(0, 1).map((item) => (
               <div className='p-2'>
-                {formItem(
-                  item.key,
-                  item.rules,
-                  item.kwargs,
-                  item.type,
-                  item.others,
-                  item.label,
-                )}
+                {formItem(item.key, item.rules, item.kwargs, item.type, item.others, item.label)}
               </div>
             ))}
           </Col>
           <Col span={8}>
             {addressFormFields.slice(2, 3).map((item) => (
               <div className='p-2'>
-                {formItem(
-                  item.key,
-                  item.rules,
-                  item.kwargs,
-                  item.type,
-                  item.others,
-                  item.label,
-                )}
+                {formItem(item.key, item.rules, item.kwargs, item.type, item.others, item.label)}
               </div>
             ))}
           </Col>
           <Col span={8}>
-            {addressFormFields.slice(1,2).map((item) => (
+            {addressFormFields.slice(1, 2).map((item) => (
               <div className='p-2'>
-                {formItem(
-                  item.key,
-                  item.rules,
-                  item.kwargs,
-                  item.type,
-                  item.others,
-                  item.label,
-                )}
+                {formItem(item.key, item.rules, item.kwargs, item.type, item.others, item.label)}
               </div>
             ))}
           </Col>
@@ -118,7 +77,7 @@ export const AddressForm = ({ onCancel, onDone, id }) => {
                   item.rules,
                   item.kwargs,
                   item.type,
-                  { selectOptions:Countries.countries[countryIndex].states, },
+                  { selectOptions: Countries.countries[countryIndex].states },
                   item.label,
                 )}
               </div>
@@ -127,23 +86,13 @@ export const AddressForm = ({ onCancel, onDone, id }) => {
           <Col span={8}>
             {addressFormFields.slice(3, 4).map((item) => (
               <div className='p-2'>
-                {formItem(
-                  item.key,
-                  item.rules,
-                  item.kwargs,
-                  item.type,
-                  item.others,
-                  item.label,
-                )}
+                {formItem(item.key, item.rules, item.kwargs, item.type, item.others, item.label)}
               </div>
             ))}
           </Col>
         </Row>
         <Row>
-          <Button
-            type='primary'
-            htmlType='submit'
-              >
+          <Button type='primary' htmlType='submit'>
             Save
           </Button>
           <div className='p-2' />
