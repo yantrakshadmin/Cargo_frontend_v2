cbd3465513982302118d7f5fe0c83e74b237f37e
Faisal Manzer <faisal_manzer@yahoo.in>
2020-06-07 13:55:19 +0530

    transport dir

--- packages/common/columns/shipperTransportDirectory.column.js
+++ packages/common/columns/shipperTransportDirectory.column.js
@@ -1,75 +1,54 @@
-const gg = [
-  {
-    index: 81,
-    Id: 82,
-    'Company Name': 'A.K.T. Cargo & Express Logistics',
-    'Contact Person Name': '-',
-    'Primary Contact Number': '9531809535',
-    'Contact Number 1': '-',
-    'Contact Number 2': '-',
-    'Contact Number 3': '-',
-    'Contact Number 4': '-',
-    'Primary Email': 'aktgroupspb@gmail.com',
-    'Secondary Email': '-',
-    Address: '# 1 Unit No.4 Mansarovar Above Dolly Gunj Port Blair South',
-    City: 'Andamans',
-    'Pin Code': 744103.0,
-    State: 'Andamans',
-    Services: 'All Over India & Andaman & Nicobar Islands',
-  },
-];
-
 export const shipperTransportDirectoryColumn = [
   {
     title: 'Company Name',
     dataIndex: 'Company Name',
     key: 'company_name',
   },
-  {
-    title: 'Contact Person Name',
-    dataIndex: 'Contact Person Name',
-    key: 'contact_person_name',
-  },
-  {
-    title: 'Primary Contact Number',
-    dataIndex: 'Primary Contact Number',
-    key: 'primary_contact_number',
-  },
-  {
-    title: 'Secondary Contact Number',
-    dataIndex: 'Contact Number 1',
-    key: 'secondary_contact_number',
-  },
-  {
-    title: 'Contact Number 4',
-    dataIndex: 'Contact Number 2',
-    key: 'secondary_contact_number',
-  },
-  {
-    title: 'Email',
-    dataIndex: 'Primary Email',
-    key: 'email',
-  },
-  {
-    title: 'Address',
-    dataIndex: 'Address',
-    key: 'qddress',
-  },
+  // {
+  //   title: 'Contact Person Name',
+  //   dataIndex: 'Contact Person Name',
+  //   key: 'contact_person_name',
+  // },
+  // {
+  //   title: 'Primary Contact Number',
+  //   dataIndex: 'Primary Contact Number',
+  //   key: 'primary_contact_number',
+  // },
+  // {
+  //   title: 'Secondary Contact Number',
+  //   dataIndex: 'Contact Number 1',
+  //   key: 'secondary_contact_number',
+  // },
+  // {
+  //   title: 'Contact Number 4',
+  //   dataIndex: 'Contact Number 2',
+  //   key: 'secondary_contact_number',
+  // },
+  // {
+  //   title: 'Email',
+  //   dataIndex: 'Primary Email',
+  //   key: 'email',
+  // },
+  // {
+  //   title: 'Address',
+  //   dataIndex: 'Address',
+  //   key: 'qddress',
+  // },
   {
     title: 'Pincode',
     dataIndex: 'Pin Code',
     key: 'pincode',
   },
-  {
-    title: 'City ',
-    dataIndex: 'city',
-    key: 'city',
-  },
-  {
-    title: 'State',
-    dataIndex: 'State',
-    key: 'state',
-  },
+  // {
+  //   title: 'City ',
+  //   dataIndex: 'city',
+  //   key: 'city',
+  // },
+  // {
+  //   title: 'State',
+  //   dataIndex: 'State',
+  //   key: 'state',
+  // },
   {
     title: 'Services',
     dataIndex: 'Services',
--- packages/web/src/screens/shipper/TransportDirectory.shipper.screen.js
+++ packages/web/src/screens/shipper/TransportDirectory.shipper.screen.js
@@ -1,23 +1,55 @@
 import React, { useState } from 'react';
-import { Row, Col, Form } from 'antd';
+import { Row, Col, Form, Button, Modal, Descriptions } from 'antd';
 import { useAPI } from '@app/common/hooks/api';
 import { shipperTransportDirectoryColumn } from '@app/common/columns/shipperTransportDirectory.column';
 import States from '@app/common/constants/statesAndCity';
-import { MasterHOC } from '../../hocs/Master.hoc';
-import { formItem } from '../../hocs/formItem.hoc';
-import { FORM_ELEMENT_TYPES } from '../../constants/formFields.constant';
+import { MasterHOC } from 'hocs/Master.hoc';
+import { formItem } from 'hocs/formItem.hoc';
+import { FORM_ELEMENT_TYPES } from 'constants/formFields.constant';
+import { ContactsFilled } from '@ant-design/icons';
+
+const initialStateCity = { city: 'Ajmer', state: 'Rajasthan' };
 
 export const TransportDirectory = () => {
   const [form] = Form.useForm();
-  const [cities, setCites] = useState(States.Andamans);
-  const [requestData, setRequestData] = useState({ city: 'Andamans', state: 'Andamans' });
+  const [modal, setModal] = useState({ visible: false, data: {} });
+  const [cities, setCites] = useState(States.Rajasthan);
+  const [requestData, setRequestData] = useState(initialStateCity);
   const { data, loading, reload } = useAPI(`/directory/`, { method: 'get', params: requestData });
 
+  const columns = [
+    ...shipperTransportDirectoryColumn,
+    {
+      title: 'Contact',
+      key: 'contact',
+      render: (row) => (
+        <Button
+          block
+          type='primary'
+          icon={<ContactsFilled />}
+          onClick={() => {
+            console.log(row);
+            setModal({ visible: true, data: row });
+          }}>
+          View Contact
+        </Button>
+      ),
+    },
+  ];
+
   const onChange = () => {
+    const newCity = form.getFieldValue('city');
     const newState = form.getFieldValue('state');
+    const isOfState = (States[newState] || []).indexOf(newCity) > -1;
+
     setCites(States[newState]);
-    setRequestData({ city: form.getFieldValue('city'), state: form.getFieldValue('state') });
+    if (isOfState) setRequestData({ city: newCity, state: newState });
+    else {
+      form.setFieldsValue({ city: null });
+      setRequestData({ city: null, state: null });
+    }
   };
+
   return (
     <Form form={form} hideRequiredMark layout='vertical' onFieldsChange={onChange}>
       <Row>
@@ -30,7 +62,10 @@ export const TransportDirectory = () => {
                 placeholder: 'Select State',
               },
               FORM_ELEMENT_TYPES.SELECT,
-              { selectOptions: Object.keys(States), formOptions: { initialValue: 'Andamans' } },
+              {
+                selectOptions: Object.keys(States),
+                formOptions: { initialValue: initialStateCity.state },
+              },
               'State',
             )}
           </div>
@@ -44,16 +79,50 @@ export const TransportDirectory = () => {
                 placeholder: 'Select City',
               },
               FORM_ELEMENT_TYPES.SELECT,
-              { selectOptions: cities, formOptions: { initialValue: 'Andamans' } },
+              { selectOptions: cities, formOptions: { initialValue: initialStateCity.city } },
               'City',
             )}
           </div>
         </Col>
       </Row>
+      <Modal
+        visible={modal.visible}
+        footer={null}
+        destroyOnClose
+        onCancel={() => setModal({ visible: false, data: {} })}>
+        <Descriptions title={modal.data['Company Name']} bordered>
+          <Descriptions.Item label='Services' span={4}>
+            {modal.data.Services}
+          </Descriptions.Item>
+          <Descriptions.Item label='Contact Person Name' span={3}>
+            {modal.data['Contact Person Name']}
+          </Descriptions.Item>
+          <Descriptions.Item label='Email' span={4}>
+            {modal.data.Email}
+          </Descriptions.Item>
+          <Descriptions.Item label='Contact Numbers' span={4}>
+            {modal.data['Contact Number']}
+            <br />
+            {modal.data['Contact Number 1']}
+            <br />
+            {modal.data['Contact Number 2']}
+          </Descriptions.Item>
+          <Descriptions.Item label='City' span={2}>
+            {modal.data.City}
+          </Descriptions.Item>
+          <Descriptions.Item label='State' span={2}>
+            {modal.data.State}
+          </Descriptions.Item>
+          <Descriptions.Item label='Address' span={4}>
+            {modal.data.Address}
+          </Descriptions.Item>
+          <Descriptions.Item label='Pincode'>{modal.data['Pin Code']}</Descriptions.Item>
+        </Descriptions>
+      </Modal>
       <MasterHOC
         title='Transport Directory'
-        columns={shipperTransportDirectoryColumn}
-        data={[]}
+        columns={columns}
+        data={JSON.parse(data || '[]')}
         loading={loading}
         refresh={reload}
         hideRightButton
