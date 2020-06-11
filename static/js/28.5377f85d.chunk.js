5966dc31dd85169fb836899d3f717d2c84fc4b3d
Faisal Manzer <faisal_manzer@yahoo.in>
2020-06-11 17:10:01 +0530

    Updates

---
+++ static/js/28.5377f85d.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonp@app/web"]=this["webpackJsonp@app/web"]||[]).push([[28],{809:function(a,e,t){"use strict";t.r(e);var n=t(0),p=t.n(n),s=t(28);e.default=function(){return p.a.createElement(s.b,{to:"/available-loads/"})}}}]);
+//# sourceMappingURL=28.5377f85d.chunk.js.map
---
+++ static/js/30.9d39e6a5.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonp@app/web"]=this["webpackJsonp@app/web"]||[]).push([[30],{810:function(e,t,p){"use strict";p.r(t);var n=p(0),s=p.n(n),a=p(28);t.default=function(){return s.a.createElement(a.b,{to:"/view-pickup-requests/"})}}}]);
+//# sourceMappingURL=30.9d39e6a5.chunk.js.map
